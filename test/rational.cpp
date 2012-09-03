96b51318aed02a01c6891d8d9f3d1eaa2f01d7ab
Morwenn Edrahir <morwenn29@hotmail.fr>
2012-09-03 17:51:38 +0200

    Tests for casts, literals and abs function.

--- test/rational.cpp
+++ test/rational.cpp
@@ -34,6 +34,8 @@ int main()
     constexpr auto r2 = rational<int>(2, 4);
     constexpr auto r3 = rational<int>(1, 3);
     constexpr auto r4 = rational<int>(5, 1);
+    constexpr auto r5 = rational<int>(-1, 2);
+    constexpr auto r6 = rational<int>(1, -2);
 
     // rational-rational comparison
     static_assert(r1 == r2, "");
@@ -42,6 +44,7 @@ int main()
     static_assert(r3 < r2, "");
     static_assert(r1 >= r2, "");
     static_assert(r3 <= r2, "");
+    static_assert(r5 == r6, "");
 
     // rational-integral comparison
     static_assert(r4 == 5, "");
@@ -56,6 +59,8 @@ int main()
     static_assert(r3 <= 1, "");
     static_assert(1 >= r3, "");
     static_assert(r1 >= -8, "");
+    static_assert(r5 <= 0, "");
+    static_assert(r6 <= 0, "");
 
     // rational-rational arithmetic operations
     static_assert(r1 + r2 == 1, "");
@@ -73,8 +78,23 @@ int main()
     static_assert(1 / r2 == 2, "");
     static_assert(r3 / 3 == rational<int>(1, 9), "");
 
+    // cast
+    static_assert(rational<int>(1, 2) == rational<long int>(1, 2), "");
+    static_assert(rational<unsigned long long>(3, 2) == rational<short>(3, 2), "");
+
     // _smath_r literal
     static_assert(2 / 3_static_r == rational<unsigned long long>(2, 3), "");
+    static_assert(1_static_r / 8 == rational<unsigned long long>(1, 8), "");
+    static_assert(3 / 5_static_r == 3_static_r / 5, "");
+
+    constexpr auto a1 = rational<int>(1, 2);
+    constexpr auto a2 = rational<int>(-3, 8);
+    constexpr auto a3 = rational<int>(6, -7);
+
+    // Math functions
+    static_assert(abs(a1) == 1 / 2_static_r, "");
+    static_assert(abs(a2) == 3 / 8_static_r, "");
+    static_assert(abs(a3) == 6 / 7_static_r, "");
 
     return 0;
 }
